b93dad854637e5a4b2a97f4d7b17481031f7910f
ghramcode18 <81458028+ghramcode18@users.noreply.github.com>
2021-11-06 10:15:25 +0200

    --

--- src/main/java/delicious/delicious/controllers/Recipe_stepsController.java
+++ src/main/java/delicious/delicious/controllers/Recipe_stepsController.java
@@ -1,14 +1,24 @@
 package delicious.delicious.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import delicious.delicious.models.Recipe_StepsModel;
+import delicious.delicious.repositories.RecipeRepo;
 import delicious.delicious.repositories.Recipe_StepsRepo;
+import delicious.delicious.services.RecipeService;
 
 @RestController
-@RequestMapping(path = {"/api/v1/Recipe_stepsController"})
+@RequestMapping(path = { "/api/v1/Recipe_stepsController" })
 public class Recipe_stepsController {
     @Autowired
-    Recipe_StepsRepo recipe_steps; 
+    Recipe_StepsRepo recipe_steps;
+
+    @Autowired
+    RecipeService recipeService;
 }
